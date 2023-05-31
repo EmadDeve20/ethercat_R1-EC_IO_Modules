d259b4e4f351c98fc3aea86cfbb28c5ca3ba0d3f
EmadDeve20 <EmadDeve20@gmail.com>
2023-05-31 11:31:12 +0330

    update for connecting to ethernet device

--- ethercat_R1-EC_IO_Modules/main.cpp
+++ ethercat_R1-EC_IO_Modules/main.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QInputDialog>
+#include <QMessageBox>
+
+//SOEM
+#include "ethercat.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+    bool ifname_is_ok;
+       // Ask for birth date as a string.
+    QString ifname = QInputDialog::getText(0, "Input dialog",
+                                            "Date of Birth:", QLineEdit::Normal,
+                                            "", &ifname_is_ok);
+
+    if (ifname_is_ok && ec_init(ifname.toStdString().c_str()))
+    {
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+    else
+    {   QMessageBox messageBox;
+        messageBox.critical(0,"Error","Can not connect with your ifname! Try with sudo");
+        messageBox.setFixedSize(500,200);
+
+        return 1;
+    }
+
+
+
 }
