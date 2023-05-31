deacf4a834ab16f89a4c76feb591eddb329d292c
EmadDeve20 <EmadDeve20@gmail.com>
2023-05-31 12:04:01 +0330

    set slot and improve the change_slave_status function

--- ethercat_R1-EC_IO_Modules/mainwindow.cpp
+++ ethercat_R1-EC_IO_Modules/mainwindow.cpp
@@ -15,7 +15,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::change_slave_status(int &slave_idx, std::string &status)
 {
-
+   ec_send_processdata();
    if (status == "INIT")
    {
         ec_slave[slave_idx].state = EC_STATE_INIT;
@@ -40,5 +40,15 @@ void MainWindow::change_slave_status(int &slave_idx, std::string &status)
         return;
    }
 
+    work_count_ethernet += ec_receive_processdata(EC_TIMEOUTRET);
 
 }
+
+void MainWindow::on_set_status_btn_clicked()
+{
+    int slave_idx = ui->slave_to_set_status_combo->currentIndex();
+    std::string status =  ui->status_combo->currentText().toStdString();
+
+    change_slave_status(slave_idx, status);
+}
+
