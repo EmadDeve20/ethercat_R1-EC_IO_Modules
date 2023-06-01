68353a0aa9578c81a72cdd7af6ece5666749def8
EmadDeve20 <EmadDeve20@gmail.com>
2023-06-01 14:47:11 +0330

    fixing TODO (add status of slaves)

--- ethercat_R1-EC_IO_Modules/monitoringwindow.cpp
+++ ethercat_R1-EC_IO_Modules/monitoringwindow.cpp
@@ -9,9 +9,21 @@ MonitoringWindow::MonitoringWindow(QWidget *parent) :
 
     ui->tableWidget->setRowCount(ec_slavecount);
 
+//    for (auto r = 0; r < ui->tableWidget->rowCount(); r++)
+//    {
+//        ui->tableWidget->setItem(r, 0, new QTableWidgetItem(tr(ec_slave[r+1].name)));
+
+//        //TODO: Fix it To Show Status
+//        for (auto c = 1; ui->tableWidget->columnCount(); c++)
+//        {
+//            ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("NONE")));
+//        }
+//    }
+
     timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(update_status_table()));
     timer->start(1);
+
 }
 
 MonitoringWindow::~MonitoringWindow()
@@ -24,43 +36,42 @@ void MonitoringWindow::update_status_table()
     for (auto r = 0; r < ui->tableWidget->rowCount(); r++)
     {
         ui->tableWidget->setItem(r, 0, new QTableWidgetItem(tr(ec_slave[r+1].name)));
+        std::string status;
 
-        //TODO: Fix it To Show Status
-//        for (auto c = 0; ui->tableWidget->columnCount(); c++)
-//        {
-//            switch (ec_slave[r+1].state)
-//            {
-//              case EC_STATE_NONE:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("NONE")));
-//                break;
-//              case EC_STATE_INIT:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("INIT")));
-//                break;
-//              case EC_STATE_PRE_OP:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("PRE_OP")));
-//                break;
-//              case EC_STATE_BOOT:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("BOOT")));
-//                break;
-//              case EC_STATE_SAFE_OP:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("SAFE_OP")));
-//                break;
-//              case EC_STATE_OPERATIONAL:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("OP")));
-//                break;
-//              case EC_STATE_ACK:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("ACK")));
-//                break;
-//              case EC_STATE_PRE_OP + EC_STATE_ERROR:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("PRE_OP-ERR")));
-//                break;
-//              case EC_STATE_SAFE_OP + EC_STATE_ERROR:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("SAFE_OP-ERR")));
-//                break;
-//              case EC_STATE_OPERATIONAL + EC_STATE_ERROR:
-//                ui->tableWidget->setItem(r, c, new QTableWidgetItem(tr("OP-ERR")));
-//                break;
-//            }
-//        }
-    }
+            switch (ec_slave[r+1].state)
+            {
+              case EC_STATE_NONE:
+                status = "NONE";
+                break;
+              case EC_STATE_INIT:
+                status = "INIT";
+                break;
+              case EC_STATE_PRE_OP:
+                status = "PRE_OP";
+                break;
+              case EC_STATE_BOOT:
+                status = "BOOT";
+                break;
+              case EC_STATE_SAFE_OP:
+                status = "SAFE_OP";
+                break;
+              case EC_STATE_OPERATIONAL:
+                status = "OP";
+                break;
+              case EC_STATE_ACK:
+                status = "ACK";
+                break;
+              case EC_STATE_PRE_OP + EC_STATE_ERROR:
+                status = "PRE_OP-ERR";
+                break;
+              case EC_STATE_SAFE_OP + EC_STATE_ERROR:
+                status = "SAFE_OP-ERR";
+                break;
+              case EC_STATE_OPERATIONAL + EC_STATE_ERROR:
+                status = "OP-ERR";
+                break;
+            }
+
+            ui->tableWidget->setItem(r, 1, new QTableWidgetItem(tr(status.c_str())));
+     }
 }
